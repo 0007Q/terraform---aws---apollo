077f736b1408a822dd31bef4ebf7f7e1ba62e22a
Dominic Colson <Dominic.Colson@servicenow.com>
2023-03-23 14:49:10 +0000

    First Push
    Working Terraform config

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,1 +1,14 @@
-some text 
+provider "aws" {
+  access_key          = var.access_key
+  secret_key          = var.secret_key
+  region              = var.region
+}
+
+terraform {
+ required_providers {
+   hashicorp-http = {
+    source = "hashicorp/http"
+    version = "~> 2.0"
+  }
+ }
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Pet Clink - Terraform template for AWS EC2 Instance Request, includes:
 
     •	VPC
     •	Network Security Group
+    .   Some more text
